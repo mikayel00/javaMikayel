834cedb934e03f01b17bb46d9add90d8f590f66d
Mikayel <mikayel.hovhannisyan27@gmail.com>
2021-10-17 17:46:58 +0400

    HOMEWORK 2 (v.2) =========================== Changelog: >Edited and optimized java2 and java4 files.

--- src/homework2/java2.java
+++ src/homework2/java2.java
@@ -4,14 +4,11 @@ public class java2 {
     public static void main(String[] args) {
         /* ---- 2 ---- */
         int x = 15;
-        int y = x % 3;
-        int z = x % 5;
-        if (y == 0 && z == 0){
+        if (x % 3 == 0 && x % 5 == 0){
             System.out.println("Numbers divide");
         }
         else {
             System.out.println("Numbers not divide");
         }
-
     }
 }
--- src/homework2/java4.java
+++ src/homework2/java4.java
@@ -4,24 +4,21 @@ public class java4 {
     public static void main(String[] args) {
         /* ---- 4 ---- */
         int a = 20;
-        int b = 10;
+        int b = 20;
         int result = 1;
 
-        result = (a > b) ? (result*=(a-b)):(result*=(a+b));
+        result = (a > b) ? (result *= (a - b)) : (result *= (a + b));
         System.out.println(result);
 
         /*
         if (a > b) {
-            result *=(a-b);
+            result *= (a - b);
             System.out.println(result);
-        }
-        else {
-            if (a < b) {
-                result *= (a + b);
-                System.out.println(result);
-            } else {
-                System.out.println("Enter not equals values!");
-            }
+        } else if (a < b) {
+            result *= (a + b);
+            System.out.println(result);
+        } else {
+            System.out.println("Enter not equals values!");
         }
         */
     }
