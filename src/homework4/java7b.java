3ac730730f986edf4057d463930fa55521369dfb
Mikayel <mikayel.hovhannisyan27@gmail.com>
2021-10-28 17:10:20 +0400

    HOMEWORK 4 (v.2.1) =========================== Changelog: >Updated homework 7b.

--- src/homework4/java7b.java
+++ src/homework4/java7b.java
@@ -9,17 +9,16 @@ public class java7b {
                             {13, 9, 11, 17, 1}};
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[j][i] + " ");
+                System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
         int[] minArray = new int[5];
         for (int i = 0; i < matrix.length; i++) {
             int min = matrix[0][i];
-            int[] temp = matrix[i];
-            for (int k = 0; k < temp.length; k++) {
-                if (min > temp[k]) {
-                    min = temp[k];
+            for (int k = 0; k < matrix[i].length; k++) {
+                if (min > matrix[k][i]) {
+                    min = matrix[k][i];
                 }
             }
             System.out.println("Minimum value of " + i + " column: " + min);
